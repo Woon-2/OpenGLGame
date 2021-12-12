1b831bb5e482a27a6469208ac43cef23102da132
MyeongWoonJang <73771162+MyeongWoonJang@users.noreply.github.com>
2021-12-13 06:53:09 +0900

    ComponentCollide 트리구조로 변경
    볼륨을 여러 단계로 나누어 충돌 검사 연산 최소화

--- include/collision.h
+++ include/collision.h
@@ -9,7 +9,9 @@ class ComponentCollide
 {
 public:
 	template < typename T >
-	using Cont = std::vector< T >;
+	using ContBB = std::vector< T >;
+	template < typename T >
+	using ContNode = std::vector< T >;
 
 	static bool will_get_desc;
 
@@ -22,7 +24,7 @@ public:
 			ss << fore_indent << typeid( *this ).name() << '\n';
 			ss << fore_indent << indent << "AABB count: " << aabbs.size() << '\n';
 			ss << fore_indent << indent << "OBB count: " << obbs.size() << '\n';
-			
+
 			ss << fore_indent << indent << "AABBs: " << '\n';
 			for ( const auto& aabb : aabbs )
 			{
@@ -34,15 +36,71 @@ public:
 			{
 				ss << obb.get_desc( fore_indent + indent ).rdbuf();
 			}
+
+			ss << fore_indent << indent << "children: " << children.size() << '\n';
+			for ( const auto& child : children )
+			{
+				ss << child.get_desc( fore_indent + indent ).rdbuf();
+			}
 		}
 
 		return ss;
 	}
 
-	Cont< AABB > aabbs;
-	Cont< OBB > obbs;
+	void add_child( const ComponentCollide& child ) { children.push_back( child ); }
+	void add_child( ComponentCollide&& child ) { children.push_back( std::move( child ) ); }
+	void push_aabb( const CCoord& base, const glm::vec3& initial_half_len, const glm::vec3& initial_movement = glm::vec3{ 0.f, 0.f, 0.f } )
+	{
+		AABB aabb;
+		aabb.init( base, initial_half_len, initial_movement );
+		aabbs.push_back( std::move( aabb ) );
+	}
+	void push_obb( const CCoord& base, const glm::vec3& initial_half_len, const glm::vec3& initial_movement = glm::vec3{ 0.f, 0.f, 0.f } )
+	{
+		OBB obb;
+		obb.init( base, initial_half_len, initial_movement );
+		obbs.push_back( std::move( obb ) );
+	}
+
+	void clear()
+	{
+		aabbs.clear();
+		obbs.clear();
+		for ( auto& child : children )
+		{
+			child.clear();
+		}
+	}
 
 	const bool collide( const ComponentCollide& other ) const
+	{
+		// 루트 레벨끼리 검사
+		if ( level_collide( other ) )
+		{
+			// 종료 조건: 양 쪽에 더이상 자식이 없을 때
+			if ( children.empty() && other.children.empty() ) return true;
+
+			for ( const auto& child : children )
+			{
+				// 내 자식과 상대가 충돌하지 않는다면 false
+				if ( !child.collide( other ) ) return false;
+			}
+
+			for ( const auto& ochild : other.children )
+			{
+				// 상대 자식과 내가 충돌하지 않는다면 false
+				if ( !ochild.collide( *this ) ) return false;
+			}
+		}
+		else return false;		// 루트 레벨에서조차 충돌이 아니면 충돌이 아닌 것
+	}
+
+private:
+	ContBB< AABB > aabbs;
+	ContBB< OBB > obbs;
+	ContNode< ComponentCollide > children;
+
+	const bool level_collide( const ComponentCollide& other ) const
 	{
 		for ( const auto& aabb : aabbs )
 		{
@@ -73,7 +131,6 @@ public:
 		return false;
 	}
 
-private:
 	static OBB make_OBB_from_AABB( const AABB& aabb )
 	{
 		OBB ret;
@@ -87,7 +144,6 @@ private:
 		ret.init( obb.coord->get_base(), obb.half_len, obb.movement );
 		return ret;
 	}
-
 };
 
 bool ComponentCollide::will_get_desc = true;
